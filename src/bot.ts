1eee8691228e7c72b4e60be85561e6631bbd6a8e
Paul <paul.castle@gmail.com>
2020-06-29 00:18:14 +0100

    Add some basic data fetching functionality with hackyjank string manips

--- src/bot.ts
+++ src/bot.ts
@@ -1,5 +1,6 @@
 import * as Discord from 'discord.js';
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv';
+import fetch from 'node-fetch';
 
 dotenv.config();
 
@@ -8,22 +9,70 @@ const bot = new Discord.Client();
 
 bot.login(TOKEN);
 
+const getDeepDives = (): Promise<string> => {
+  return new Promise(async resolve => {
+    const response = await fetch('https://www.reddit.com/r/DeepRockGalactic/hot/.json?limit=0');
+    const responseBody = await response.json();
+
+    const f = responseBody.data.children.filter(
+      (element: IRedditListingChild) =>
+        element.data.stickied && element.data.title.match('Weekly Deep Dives Thread'),
+    );
+
+    resolve(formatDeepDive(f[0].data));
+  });
+};
+
+const formatDeepDive = (postObject: IRedditThread): string | undefined => {
+  const regex = new RegExp(/\*{2}Deep Dive\*{2}(.*)\*{2}Elite Deep Dive\*{2}/is);
+  let details = postObject.selftext.match(regex);
+
+  if (!details) {
+    return;
+  }
+
+  const detailsExploded = details[0].split('|'); // Remove any double line breaks
+  // /(\n){2}/
+
+  // Strip junk strings
+  const exp = detailsExploded.map(
+    ele => ele.split('**').join('').replace('Elite Deep Dive', '').trim(), // hacky ugh
+  );
+
+  const str = `
+  \`\`\`
+    Deep Dive '${exp[1]}' @ ${exp[2]}
+    ------------------------------
+    Stage ${exp[12]}: Objectives: ${exp[13]} + ${exp[14]}. Warnings: ${exp[15]}. Anomalies: ${exp[16]}
+    Stage ${exp[17]}: Objectives: ${exp[18]} + ${exp[19]}. Warnings: ${exp[20]}. Anomalies: ${exp[21]}
+    Stage ${exp[22]}: Objectives: ${exp[23]} + ${exp[24]}. Warnings: ${exp[25]}. Anomalies: ${exp[26]}
+  \`\`\`
+  `;
+
+  return str;
+};
+
 bot.on('ready', () => {
   console.info(`Logged in as ${bot.user.tag}!`);
 });
 
-
-bot.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-    msg.channel.send('pong');
-
-  } else if (msg.content.startsWith('!kick')) {
-    if (msg.mentions.users.size) {
-      const taggedUser = msg.mentions.users.first();
-      msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
-    } else {
-      msg.reply('Please tag a valid user!');
-    }
+bot.on('message', async msg => {
+  if (msg.content === '!deepdives') {
+    const deepDiveInfo = await getDeepDives();
+    msg.channel.send(deepDiveInfo);
   }
 });
+
+interface IRedditThread {
+  stickied: boolean;
+  permalink: string;
+  selftext: string;
+  url: string;
+  created_utc: number;
+  author: string;
+  title: string;
+}
+
+interface IRedditListingChild {
+  data: IRedditThread;
+}
