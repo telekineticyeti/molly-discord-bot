bdaca68c6a6333c3c15e0e956af33eb947012915
Paul <paul.castle@gmail.com>
2020-07-12 22:29:52 +0100

    schedule test

---
+++ src/schedule.ts
@@ -0,0 +1,42 @@
+// import * as cron from 'node-cron';
+// import {FlightRising} from './classes/fllght-rising.class';
+
+// const fr = new FlightRising();
+
+/**
+ *
+ * schedule [command] [time]
+ *
+ * for each job in jobs schedule [command] [time]
+ *
+ * [
+ *  {
+ *    time: '* * * * *',
+ *    destinations: [
+ *      { guildId: '', channelId: '' }
+ *    ]
+ *    command: fr.composeBonusMessage()
+ *  }
+ * ]
+ *
+ */
+
+// const testConfig = [
+//   {
+//     time: '* * * * *',
+//     destinations: [{guildId: '', channelId: ''}],
+//     command: 'fr.composeBonusMessage()',
+//   },
+// ];
+
+// const configJson = JSON.stringify(testConfig);
+
+// const config = JSON.parse(configJson);
+
+// console.log(config);
+
+// class ScheduleCommands {
+
+// }
+
+// export =
